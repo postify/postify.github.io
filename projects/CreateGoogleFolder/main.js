14f2914554c46ed3a90867dd0993a180afa4083b
sabbakilam <pitdrop@gmail.com>
2016-10-16 02:21:23 +0000

    mvc audio

--- projects/CreateGoogleFolder/main.js
+++ projects/CreateGoogleFolder/main.js
@@ -4,6 +4,7 @@ var view = $;
 //================//
 //====| VIEW |====//
 //================//
+//---| attach all DOM-related items |---//
 view.holder =  // outer div of the project
 view.app =     // the app div
 view.msg = // place where test results are explained
@@ -12,22 +13,22 @@ view.slider = //range type input
 //view.etc =   // describe each DOM object to be accessed in this app project
 "domObjects";// dummy string variable
 view.attachDomObjects();
-//---| attach all DOM-related items |---//
-view.initialize = function initialize(){
-    view.adjustRem(8, 20);
-    //etc.
-};
+
 
 //=================//
 //====| MODEL |====//
 //=================//
+//Anugama - Silk Tree.mp3
 var model = {
      windowWidth: window.innerWidth
     ,windowHeight: window.innerHeight
+    ,player: document.createElement('audio')
+    ,musicSource: "https://HussMalik.github.io/music/Chicago - Introduction.mp3"
     ,resized: false
     ,pendingEvents: []
     ,updateModelBusy: false
     ,eventCount: 0
+    ,sliderMaximum: 10000
 };
 
 //======================//
@@ -61,13 +62,28 @@ var controller = {
             $(view.msg).html(evt.target.id + ": "+ evt.type);
             view.folderName.value = model.eventCount;            
         }
-        
+        if(evt.type === "timeupdate" && evt.target.id === 'player'){
+            if(model.player.duration !== 0 && !isNaN( model.player.duration) ){
+                let fraction = model.player.currentTime / model.player.duration;
+                $.slider.value = fraction * model.sliderMaximum;   
+            }
+        }
+        if(evt.type === "input" && evt.target.id === "slider"){
+           if(model.player.duration !== 0 && !isNaN( model.player.duration) ){
+               let fraction = 1*$.slider.value / model.sliderMaximum;
+               model.player.currentTime = model.player.duration * fraction;
+           }
+        }
+        if(evt.type === 'ended' && evt.target.id === 'player'){
+            $.player.currentTime = 0;
+            $.slider.value = 0;
+        }
         if(model.resized){
             $.adjustRem();
         }
     }
-    ,monitoredEvents: [
-        ,"keydown"
+    ,monitoredDomEvents: [
+         "keydown"
         ,"keyup"
         ,"mousedown"
         ,"mouseup"
@@ -77,7 +93,36 @@ var controller = {
         ,"focus"
         ,"mousemove"
         ,"input"
+        //music player events
+
     ]
+    ,monitoredAudioEvents: [
+         "timeupdate"
+        ,"ended"
+        ,"volumechange"
+        ,"progress"
+        ,"error"
+        
+    ]
+};
+
+view.initialize = function initialize(){
+    view.adjustRem(8, 30);
+    $.slider.value = 0;
+    $.player = model.player;
+    $.player.src = model.musicSource;
+    $.player.setAttribute("controls", "controls");
+    $.player.setAttribute("id", "player");
+    $.player2 = document.createElement('audio');
+    $.player2.src = model.musicSource;
+    $.player2.volume = 0.5;
+    $.player.play();
+    /*
+    setTimeout(function(){
+        $.player2.play();
+    },48000);
+    */
+    //etc.
 };
 
 //====| app "starts" here |====//
@@ -85,11 +130,25 @@ view(window).on("load", function(){
     
     view.initialize();
     
-    controller.monitoredEvents.forEach(eventType=>{
+    controller.monitoredDomEvents.forEach(eventType=>{
         $(window).on(eventType, e=>{
             e.stopPropagation(); // prevent target from seeing it own event
             controller.registerEvent(e);
         }, true); // "capture" the event early (on the way down)
      });
+     
+    controller.monitoredAudioEvents.forEach(eventType=>{
+        $($.player).on(eventType, e=>{
+            e.stopPropagation(); // prevent target from seeing it own event
+            controller.registerEvent(e);
+        }, true); // "capture" the event early (on the way down)
+     }); 
+     
+    controller.monitoredAudioEvents.forEach(eventType=>{
+        $($.player2).on(eventType, e=>{
+            e.stopPropagation(); // prevent target from seeing it own event
+            controller.registerEvent(e);
+        }, true); // "capture" the event early (on the way down)
+     });      
 });
 //====| app "ends" here |====//
