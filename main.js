b1bf6fbb698ac0d79e989b9a1b5152a458248674
sabbakilam <pitdrop@gmail.com>
2016-10-23 14:52:24 +0000

    create folder

--- main.js
+++ main.js
@@ -1,56 +1,161 @@
 /*global $*/
-//====| Gather DOM elements, then make a global data model for the app |====//
-$.holder =  // outer div of the project
-$.app =     // the app div
-$.btnAuth = // buttonto authorize google drive
-//$.etc =   // describe each DOM object to be accessed in this app project
+/*global gapi*/
+var view = $;
+//================//
+//====| VIEW |====//
+//================//
+//---| attach all DOM-related items |---//
+view.holder =  // outer div of the project
+view.app =     // the app div
+view.msg = // place where test results are explained
+view.folderName = // user's type sht name of the folder here
+//view.etc =   // describe each DOM object to be accessed in this app project
 "domObjects";// dummy string variable
-$.attachDomObjects();
+view.attachDomObjects();
+view.initialize = function initialize(){
+    view.adjustRem(8, 30);
+};
 
-//====| Make the global data model object that holds the app's state variables |====//
+
+//=================//
+//====| MODEL |====//
+//=================//
+//Anugama - Silk Tree.mp3
 var model = {
-     windowWidth: window.innerWidth
-    ,windowHeight: window.innerHeight
-    ,resized: false
-    
-};
-$.initialize = function initialize(){
-    $.adjustRem(1, 100);
-    //etc.
+     resized: false 
+    ,pendingEvents: []
+    ,updateModelBusy: false
+    ,sliderMaximum: 10000
+    //drive info
+    ,folderName: ""
+    ,folderId: ""
 };
 
-//====| app "starts" here |====//
-$(window).on("load", function(){
-    $.initialize();
-    //====| write short handlers for dom events: only change model data |====//
-    // Note: handlers should only change model flags and model data, not the DOM.
-    $(window).on("resize", function(){
-        model.windowWidth = window.innerWidth;
-        model.windowHeight = window.innerHeight;
-        model.resized = true;
-    });
-    $(window).on("mousedown", function(e){
-        var targetId = e.target.id;
-        if(targetId === "btnAuth"){document.location.assign("quickstart.html");}
-    });
+//======================//
+//====| CONTROLLER |====//
+//======================//
+var controller = {
+     registerEvent: function registerEvent(e){
+         model.pendingEvents.push(e);
+         while(model.pendingEvents.length !== 0 && !model.updateModelBusy){
+            let nextEventObject = model.pendingEvents.shift();
+            controller.updateModel(nextEventObject);
+         }
+     }
+    ,updateModel: function updateModel(e){
+        model.updateModelBusy = true;        
+        //---------------------------//
+        if(e.type === "resize"){
+            model.resized = true;
+        }
+        //====| KEYUP for drive name |====//
+        if(e.type === "keyup"){
+            if(e.keyCode === 13){
+                model.folderName = view.folderName.value.trim();
+                gapi.client.load('drive', 'v3', this.createFolder);
+                //this.createFolder();
+            }
+        }
+        //---------------------------//
+        controller.updateView(e);            
+        model.updateModelBusy = false;
 
-    //=====| update the view (GUI) with this polling timer |====//
-    /* 
-        Note: the real handlers are in updateView,
-        which checks the model for changes, and updates the GUI accordingly
-    */
-    setInterval(updateView, 16.667); // polls the model at 60 frames/second
-    function updateView(){
+    }
+    ,updateView: function updateView(evt){
+        if(!(evt.type === "mousemove" && evt.target.id === 'slider')){        
+            $(view.msg).html(evt.target.id + ": "+ evt.type);
+        }
         if(model.resized){
             $.adjustRem();
-            model.resized = false;
         }
-        if(true){}
-        if(true){}
-        if(true){}
-        if(true){}
-        if(true){}
-        //etc.
-    };
-});
-//====| app "ends" here |====//
+
+        /*
+        if(evt.type === "timeupdate" && evt.target.id === 'player'){
+            if(model.player.duration !== 0 && !isNaN( model.player.duration) ){
+                let fraction = model.player.currentTime / model.player.duration;
+                //etc.
+            }
+        }
+        if(evt.type === "input" && evt.target.id === "slider"){
+           if(model.player.duration !== 0 && !isNaN( model.player.duration) ){
+               let fraction = 1*$.slider.value / model.sliderMaximum;
+               //etc.
+            }
+        }
+        */
+
+    }
+    ,createFolder: function createFolder(){
+        //http://stackoverflow.com/questions/12698541/how-to-create-a-folder-on-google-drive-using-javascript
+       var request = gapi.client.request({
+           'path': '/drive/v3/files/',
+           'method': 'POST',
+           'headers': {
+               'Content-Type': 'application/json'
+           },   
+           'body':{
+               "title" : model.folderName,
+               "mimeType" : "application/vnd.google-apps.folder",
+           }
+       });
+       request.execute( function(response) { 
+            alert(response);
+       });
+    }
+    ,monitoredDomEvents: [
+         "keydown"
+        ,"keyup"
+        ,"mousedown"
+        ,"mouseup"
+        ,"mouseover"
+        ,"mouseout"
+        ,"resize"
+        ,"focus"
+        ,"mousemove"
+        ,"input"
+        //music player events
+    ]
+    ,monitoredAudioEvents: [
+         "timeupdate"
+        ,"ended"
+        ,"volumechange"
+        ,"progress"
+        ,"error"
+    ]
+    
+};
+
+//=====================//
+//====| APP START |====//
+//=====================//
+view(window).on("load", function(){
+    
+    view.initialize();
+    /**
+        After initializing:
+        1.) Catch all user-generated event objects
+            of interest at a high level of the DOM element tree. Here we 
+            catch them all at the window (global) object.
+        2.) Catch all audio player generated events at the player.
+        3.) That's it. Let the controller do all the work:
+                a.) Register all event objects in a "wait your turn" queue (an array).
+                b.) RegisterEvent calls upddateModel, which "calls back" updateView.
+    */
+    controller.monitoredDomEvents.forEach(eventType=>{
+        $(window).on(eventType, e=>{
+            e.stopPropagation(); // prevent target from seeing its own event
+            controller.registerEvent(e);
+        }, true); // "capture" the event early (on the way down the DOM tree)
+     });
+     /*
+    controller.monitoredAudioEvents.forEach(eventType=>{
+        $($.player).on(eventType, e=>{
+            e.stopPropagation(); // prevent target from seeing its own event
+            controller.registerEvent(e);
+        }, true); // "capture" the event early (on the way down the DOM tree)
+     }); 
+     */
+     
+});//================//
+//====| APP END |====//
+//===================//
