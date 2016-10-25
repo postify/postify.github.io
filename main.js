cc32efbd389da879dd8806f7c06cd13283e844e9
sabbakilam <pitdrop@gmail.com>
2016-10-25 23:17:45 +0000

    new auth handling-52

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ var controller = {
                 $($.msg).html("You are NOT authorized.");
                 $($.btnLogin).styles("display: inline-block");
                 $($.shroud).styles("display: block");                
-                controller.authorizeUser(true, controller.handleAuthResult);
+                controller.authorizeUser(false, controller.handleAuthResult);
             }
         }
     ,registerEvent: function registerEvent(e){
