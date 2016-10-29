40982bf7793aab20c0bf93728306e0e5c374099d
sabbakilam <pitdrop@gmail.com>
2016-10-29 23:05:34 +0000

    generalized authorization-8

--- main.js
+++ main.js
@@ -189,13 +189,17 @@ var userdrive = {
     clientId: '152061817422-vkr6fn8jtikb6lhkmqtjfja1o9uooseb.apps.googleusercontent.com'
     ,scope: 'https://www.googleapis.com/auth/drive.appfolder https://www.googleapis.com/auth/drive.appdata'
     ,showMetaData: function showMetaData(arg){
-        alert(arg);
+        this.driveAction(function(arg){
+            alert(arg);            
+        });
     }
     ,mkDir: function mkDir(arg){
-        alert(arg);        
+        this.driveAction(function(arg){
+            alert(arg);             
+        });
     }
     ,rmFile: function rmFile(arg){
-        this.driveAction(function(){
+        this.driveAction(function(arg){
             alert(arg);             
         });
     }
