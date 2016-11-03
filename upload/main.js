65cf837832c0dc115836808c41c7a491a0d4b3af
sabbakilam <pitdrop@gmail.com>
2016-11-03 20:25:29 +0000

    upload login screen-4

---
+++ upload/main.js
@@ -0,0 +1,46 @@
+//====================//
+//=====| MODEL |======//
+//====================//
+var model = {
+    USERNAME: "",
+    PASSWORD: ""
+};
+
+
+//====================//
+//======| VIEW |======//
+//====================//
+var view = {};
+
+//=========================//
+//=====| CONTROLLER |======//
+//=========================//
+var ctrl = {
+    init: function init(){},
+    eventList: [
+        "click"
+        ,"change"
+        ,"mousedown"
+        ,"mouseup"
+    ],
+    eventHandler: function eventHandler(e){
+        e.stopPropagation();
+        alert(e.type);   
+    }
+};
+
+
+/**
+//======================//
+//=====| STARTUP |======//
+//======================//
+*/
+window.addEventListener("load", function(){
+    ctrl.init();
+    ctrl.eventList.forEach(function(eventType){
+        window.addEventListener(eventType, ctrl.eventHandler, true);
+    });
+    
+});//====================//
+//======| END of App |===//
+//=======================//
