0130f325a0d96e1f3a2fb84a498d5b8af8f9c0e6
sabbakilam <pitdrop@gmail.com>
2016-12-25 01:27:25 +0000

    local drive client

---
+++ TestDrive.1/a.js
@@ -0,0 +1,92 @@
+/*
+    a.js is an attempt to wrap our own api around the JavaScript Google api
+    to simplify using the Google Drive api
+*/
+
+/*global gapi*/
+/*global m*/
+/*global v*/
+/*global c*/
+
+var a = {};
+a.authToken = {
+    client_id: '637721329784-nm1n6dd1m05hgbc2o10e8hjj2md5ft59.apps.googleusercontent.com',
+    scope: 'https://www.googleapis.com/auth/drive.appfolder',
+    immediate: false
+};
+a.authorized = false;
+
+a.createFolder = function(folderpath="music"){
+    //assume immediate authorization
+    //if not, ask permission
+    alert(folderpath);
+};
+
+a.showFiles = function (filepath ="dummy/path"){
+    //-----| callback for showing files |----//
+    function showFiles(){
+        alert("You are Authorized to SHOW FILES: " + filepath);
+    }
+    //--------------------------------------------//
+    a.authorizeAndPerform(showFiles);
+};
+
+a.getFile = function(filepath = "dummy/path/filename"){
+    //-----| callback for getting file |----//
+    function getFile(){
+        alert("You are Authorized to GET A FILE: " + filepath);
+    }
+    //--------------------------------------------//
+    a.authorizeAndPerform(getFile);
+};
+a.saveFile = function(filepath = "dummy/path/filename"){
+
+    //-----| callback for saving file |----//
+    function saveFile(){
+        alert("You are Authorized to SAVE A FILE: " + filepath);
+    /*
+        1. Assume user is authenticated (immediate = true)
+        2. Verify an existing music folder
+            if not, create a folder
+        3. save file to music folder
+    */        
+    }
+    //--------------------------------------------//
+    a.authorizeAndPerform(saveFile);
+};
+a.deleteFile = function(filepath = "dummy/path/filename"){
+
+    //-----| callback for deleting a file |----//
+    function deleteFile(){
+        alert("You are Authorized to DELETE A FILE: " + filepath);
+    }
+    //--------------------------------------------//
+    a.authorizeAndPerform(deleteFile);
+};
+a.handleAuthResult = function(authResult, callBack){
+    if(authResult && ! authResult.error){
+        //alert("you are authorized.");
+        a.authorized = true;
+        v.authMsg.innerHTML = "";
+        callBack();
+    }
+    else{
+        a.authorized = false;
+        a.authorizeAndPerform(callBack);       
+    }
+};
+a.authorizeAndPerform = function authorizeAndPerform(callBack){
+    if(a.authorized){
+        a.authToken.immediate = true;
+    }
+    else{
+        a.authToken.immediate = false;        
+    }
+    gapi.auth.authorize(a.authToken, function(authResult){
+        a.handleAuthResult(authResult, callBack);
+    });        
+};
+//aliases, etc.
+a.makeFolder = a.createFolder;
+
+
---
+++ TestDrive.1/main.js
@@ -0,0 +1,106 @@
+//===================================//
+//==========| START OF APP|==========//
+//===================================//
+
+// "a" is our api object from file a.js loaded with index.html
+/*global a*/
+
+//=============================//
+//=========| MODEL |===========//
+//=============================//
+var m = {
+    CLIENT_ID: '152061817422-vkr6fn8jtikb6lhkmqtjfja1o9uooseb.apps.googleusercontent.com',
+    SCOPE: 'https://www.googleapis.com/auth/drive.appfolder',
+    IMMEDIATE: false,
+    authToken: {
+        client_id: this.CLIENT_ID,
+        scope: this.SCOPE,
+        immediate: this.IMMEDIATE
+    },
+    musicFolderExists: false,
+    songListJson: {},
+    savedFile: "", //most recently saved music file (which hopefully contains a picture)
+};
+
+//=============================//
+//==========| VIEW |===========//
+//=============================//
+//make v (our view) an alias for our api a
+var v = a; // "a" is our api object from file a.js loaded with index.html
+v.id = function(idString){
+    return document.getElementById(idString);
+};
+v.btnShowFiles=v.id("btnShowFiles");
+v.btnSaveFile=v.id("btnSaveFile");
+v.btnGetFile=v.id("btnGetFile");
+v.btnDeleteFile=v.id("btnDeleteFile");
+v.msg = v.id("msg");
+v.app = v.id("app");
+v.authMsg = v.id("authMsg");
+v.window = this;
+v.window.id = "window";
+
+
+//=============================//
+//=======| CONTROLLER |========//
+//=============================//
+var c = {};
+c.initialize = function initialize(){
+    //get authorized to verify a folder on the site named "music"
+    //else get authorized to create one, and ...
+    //welcome noew user
+    
+};
+//------------------------//
+//-----| UPDATE MODEL |---//
+//------------------------//
+c.updateModel = function(e, updateView){
+    var target = e.target;//source of the event
+    var id = target.id; //id of event source
+    var type = e.type; //type of event
+    showEvent(e);
+    updateView(e);
+    m.musicFolderExists;
+    //----| helpers |----//
+    function showEvent(e){
+        v.msg.innerHTML = `${id}, ${type}`;
+    }
+};
+//------------------------//
+//-----| UPDATE VIEW |----//
+//------------------------//
+c.updateView = function(e){
+
+    if (e.target === v.btnShowFiles && e.type === "mousedown"){
+        v.showFiles("asdfasdfsadfafasdf");
+    }
+    else if (e.target === v.btnSaveFile && e.type === "mousedown"){
+        v.saveFile();
+    }
+    else if (e.target === v.btnGetFile && e.type === "mousedown"){
+        v.getFile();
+    }
+    else if (e.target === v.btnDeleteFile && e.type === "mousedown"){
+        v.deleteFile("kill this file!!!");
+    }
+};
+
+//=============================//
+//=========| STARTUP|==========//
+//=============================//
+window.onload = function(){
+    c.initialize();
+    
+    ["mousedown",
+     "mouseup",
+     "mouseover",
+     "mouseout",
+     "resize"].forEach(eventType=>{
+        window.addEventListener(eventType, function(event){
+           c.updateModel(event, c.updateView);
+        });        
+    });
+};
+//=================================//
+//==========| END OF APP|==========//
+//=================================//
