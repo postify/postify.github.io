acbbcde885b9a52f36779abc14c30dd32b8e8b52
sabbakilam <pitdrop@gmail.com>
2016-12-27 17:49:25 +0000

    googledrive Buttons

--- projects/FileButtons/main.js
+++ projects/FileButtons/main.js
@@ -9,12 +9,10 @@ var m = {};
 //====================//
 /*global a*/ 
 var v = a; //'a' is our personal api library
-/*
-v. = //
-v. = //
-v. = //
-v. = //
-*/
+v.imageFileElement = //
+v.musicFileElement = //
+v.btnImageFile = //
+v.btnMusicFile = //
 ""; //dummy DOM element
 v.attachDomObjects();
 
@@ -24,8 +22,23 @@ v.attachDomObjects();
 //=========================//
 var c = {};
 c.initialize = function initialize(){};
-c.updateModel = function updateModel(event, updateView){};
-c.updateView = function updateView(){};
+c.updateModel = function updateModel(event, updateView){
+    //code here, then ...
+    updateView(event);
+};
+c.updateView = function updateView(event){
+    var source = event.target;
+    var id = source.id;
+    var type = event.type;
+
+    // call file browser indirectly
+    if(source == v.btnMusicFile){
+        v.musicFileElement.click();
+    }
+    else if(source == v.btnImageFile){
+        v.imageFileElement.click();
+    }
+};
 
 
 //======================//
@@ -33,7 +46,7 @@ c.updateView = function updateView(){};
 //======================//
 c.initialize();
 window.addEventListener("load", function(){
-    ["click","change"].forEach(eventType=>{
+    ["mousedown","click","change"].forEach(eventType=>{
         window.addEventListener(eventType, function(event){
             c.updateModel(event, c.updateView);
         });
