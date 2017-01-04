46413b4deae44e7af6f98e452cb4c01e39b01db1
sabbakilam <pitdrop@gmail.com>
2017-01-04 04:20:10 +0000

    associated filenames-5

--- a.js
+++ a.js
@@ -227,7 +227,7 @@ a.uploadFile = function uploadFile( CONTENT, filename, parentFolder ){
             },
             'body': multipartRequestBody});
         request.execute(function(file, raw){
-            console.log(`'${file.name}', ${file.id}, ${file.description}`);
+            console.log(`'${file.title}', ${file.id}, ${file.description}`);
             v.showAllButtons();    
             v.clearAllText();
             v.showFiles();
