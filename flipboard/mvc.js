14c72f2873c4332848cb698a8270e6a2c709ca5b
sabbakilam <pitdrop@gmail.com>
2017-02-17 01:14:56 +0000

    nonlinear auto flip faster-6

--- flipboard/mvc.js
+++ flipboard/mvc.js
@@ -13,8 +13,8 @@ m.started = m.DOWN;
 m.direction = m.UP;
 m.firmlyPressed = false;
 m.pressed = false;
-m.currentAngle = 1; // in degrees
-m.currentY = 0;
+m.currentAngle = 0; // in degrees
+m.currentY = window.innerHeight;
 m.priorY = 0;
 m.appWidthMax = 500; // in pixels
 m.autoY = 0;
@@ -22,7 +22,7 @@ m.autoY = 0;
 //constants in camel case:
 m.flipTransitionTime = 5; //10; //in milliseconds
 //m.stepAngle = 4; //in degrees
-m.linearStepFraction = 1/100//;1/66;
+m.linearStepFraction = 1/66//;1/66;
 m.flipTimerInterval = (m.stepAngle / 180) * m.flipTransitionTime; // in milliseconds:
 m.flipperTimerId = 0 ; //id of flipper interval timer for auto flipping
 m.debounceTimerId = 0 ;
@@ -169,10 +169,9 @@ c.flipAutomatically = function flipAutomatically(eventObject){
                 a.) if angle > 60, keep going up
                 b.) if angle <= 60, fall back down
         */
-    },1 );//m.flipTimerInterval
+    }, m.flipTimerInterval );//m.flipTimerInterval
     //--------------------------//
-
-}
+};
 
 
 c.moveFlipperWithFinger = function(){
